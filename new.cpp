e6e854732495ad30eda9619a11e7150ba139e6d2
yashiro <qmyashiro@126.com>
2016-06-27 10:45:37 +0800

    newone

---
+++ new.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: new.cpp
+	> Author: Qi
+	> Mail: qmyashiro@126.com
+	> Created Time: Mon 27 Jun 2016 10:44:53 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    return 0;
+}
